04ae23f41a5cfb744c526b03555bf5e96b3d740f
Calvin <calvin.gedenk@hotmail.com>
2013-05-08 12:51:04 +0200

    Add a full_title helper

--- rails_projects/first_app/config/initializers/secret_token.rb
+++ rails_projects/first_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = 'd6c731ddc7d0aacf5846f426f6324b67ac615f29fa60da982c8340bf53600a1f0fcd42742cb1d2c54c9b1e1623cb77dd1e8158f2e8e0ec8e1ce590a435bfedf7'
+FirstApp::Application.config.secret_token = '74af4fe77c21e5f492ccd1b220bdb4f855b82b0c37265f090433a9aea9763980c9cb07a03d168bd78809658bc1f672774bdec69bae6dc5fff631c72d8e3b66a3'
